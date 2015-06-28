857370b1a49623d5e2af6b505414db129ede0bed
Benjie Jiao <benjiao12@gmail.com>
2015-06-29 04:08:54 +0800

    Document tests

--- tests/test_python_redisqueue.py
+++ tests/test_python_redisqueue.py
@@ -7,18 +7,32 @@ import python_redisqueue
 class BasicTests(TestCase):
 
     def setUp(self):
+        """
+        Generate a queuename for this test (with random uuid)
+        """
         self.queuename = "redisqueuetest:%s" % uuid.uuid4()
         self.queue = python_redisqueue.RedisQueue(self.queuename)
 
     def test_length_on_empty(self):
+        """
+        length() must return 0 if queue is empty/not yet in redis
+        """
+
         l = self.queue.length()
         self.assertEqual(0, l)
 
     def test_dequeue_on_empty(self):
+        """
+        deq() must return None if no item has been dequeued
+        """
         r = self.queue.deq()
         self.assertEqual(None, r)
 
     def test_enqueue_int(self):
+        """
+        Enqueueing and dequeueing an integer must return <type 'int'> type
+        """
+
         self.queue.enq(1)
 
         l = self.queue.length()
@@ -29,6 +43,9 @@ class BasicTests(TestCase):
         self.assertEqual(1, r)
 
     def test_enqueue_string(self):
+        """
+        Enqueueing and dequeueing a string must return <type 'str'> type
+        """
         self.queue.enq("Test String")
 
         l = self.queue.length()
@@ -39,6 +56,9 @@ class BasicTests(TestCase):
         self.assertEqual("Test String", r)
 
     def test_enqueue_dict(self):
+        """
+        Enqueueing and dequeueing a python dict() object must return <type 'dict'> type
+        """
         d = {"Test Key 1": "Test Value 1",
              "Test Key 2": "Test Value 2"}
 
@@ -52,6 +72,9 @@ class BasicTests(TestCase):
         self.assertEqual(d, r)
 
     def test_ordering(self):
+        """
+        Order must be preserved. Should strictly follow first-in first-out.
+        """
         self.queue.enq("First")
         self.queue.enq("Second")
         self.queue.enq("Third")
@@ -61,6 +84,10 @@ class BasicTests(TestCase):
         self.assertEqual("Third", self.queue.deq())
 
     def test_purge(self):
+        """
+        purge() must empty a queue
+        """
+
         self.queue.enq("First")
         self.queue.enq("Second")
         self.queue.enq("Third")
