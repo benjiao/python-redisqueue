681406d33835df369c68a032d12bd7f8b6439062
Benjie Jiao <benjiao12@gmail.com>
2015-06-29 00:37:56 +0800

    Fixed long description, version

--- setup.py
+++ setup.py
@@ -3,16 +3,26 @@ python-redisqueue
 """
 from setuptools import setup
 
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md')
 
 setup(
     name='redisqueue',
-    version='0.1.0dev1',
+    version='0.1.0a1',
     url='http://code.benjie.me/python-redisqueue',
     license='BSD',
     author='Benjie Jiao',
     author_email='benjiao12@gmail.com',
     description='An easy to use redis-based queue implementation',
-    long_description=__doc__,
+    long_description=long_description,
     py_modules=['redisqueue.redisqueue'],
     zip_safe=False,
     include_package_data=True,
